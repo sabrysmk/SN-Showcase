2f227e598281ca56a7eca3fdc850c5de8fd4feba
sabry <sabry.smk@gmail.com>
2016-02-23 14:58:26 +0300

    Initial Commit

---
+++ SN-Showcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SN-Showcase
+//
+//  Created by Александр Сабри on 23/02/16.
+//  Copyright © 2016 Александр Сабри. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
