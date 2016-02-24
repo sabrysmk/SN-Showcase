92da3a704c965f43e8d81327864343346fd8e277
sabry <sabry.smk@gmail.com>
2016-02-25 00:44:27 +0300

    Finish Post TableViewCell

---
+++ SN-Showcase/FeedVC.swift
@@ -0,0 +1,38 @@
+//
+//  FeedVC.swift
+//  SN-Showcase
+//
+//  Created by Александр Сабри on 25/02/16.
+//  Copyright © 2016 Александр Сабри. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+        
+    }
+    
+    
+}
---
+++ SN-Showcase/PostCell.swift
@@ -0,0 +1,35 @@
+//
+//  PostCell.swift
+//  SN-Showcase
+//
+//  Created by Александр Сабри on 25/02/16.
+//  Copyright © 2016 Александр Сабри. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var showcaseImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func drawRect(rect: CGRect) {
+        profileImg.layer.cornerRadius = profileImg.frame.size.width / 2
+        profileImg.clipsToBounds = true
+        
+        showcaseImg.clipsToBounds = true
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
+
